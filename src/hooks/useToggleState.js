115660f84c3d2f43f0d5b7436daf0ba4d76ea134
Harshal Savaliya <hetsavaliya19@gmail.com>
2023-05-04 22:42:55 +0530

    Adding Dependencies and Created Context Api

---
+++ src/hooks/useToggleState.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function useToggleState(defaultVal) {
+  const [state, setState] = useState(defaultVal);
+
+  const toggleState = () => {
+    setState(!state);
+  };
+
+  return [state, toggleState];
+}
+
+export default useToggleState;
